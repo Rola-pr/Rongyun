e8f6d9b793b93ab28be28b491344cc48c9830a28
Peirong Zhang <y.peirong.zhang@gmail.com>
2022-12-05 17:54:55 +0800

    Initial commit

---
+++ src/main/java/com/springbootuse/SpringBootUseApplication.java
@@ -0,0 +1,13 @@
+package com.springbootuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUseApplication.class, args);
+    }
+
+}
